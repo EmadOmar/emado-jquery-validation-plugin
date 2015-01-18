caf69abbe4e23327b413f62b3004fb5cf8201d55
Emad Omar <emad2030@gmail.com>
2015-01-18 08:58:39 +0300

    Testing Git Repository

--- js/jquery-emado_validation.js
+++ js/jquery-emado_validation.js
@@ -53,7 +53,7 @@
                 $(this).keyup(nozzha_validate);
                 $(this).change(nozzha_validate);
                 $(this).blur(nozzha_validate);
-                // nozzha_validate(); -- testd
+                // nozzha_validate(); -- best
             });
         };
     }(jQuery)); 
